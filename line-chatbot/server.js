c8c442e622ec238ccf0aadf7716f9ea6c8ed95fc
unknown <lo0kpad@hotmail.com>
2021-01-14 22:51:43 +0700

    :cherry_blossom: [Added] Pricetable of bus n147&680101&720 :cherry_blossom:

--- line-chatbot/server.js
+++ line-chatbot/server.js
@@ -9,7 +9,7 @@ const passport = require('passport');
 const { functionmenu1, menu1ans } = require('./menu/functionmenu1')
 const { functionmenu2, custompoint } = require('./menu/functionmenu2')
 const { functionmenu3, timebus, resulttimebus } = require('./menu/functionmenu3')
-const { functionmenu4, selectnumbus, cost140, cost141, cost76 , cost105, cost558 } = require('./menu/functionmenu4')
+const { functionmenu4, selectnumbus, cost140, cost141, cost76 , cost105, cost558, cost147, costminibus, cost68, cost101, cost720 } = require('./menu/functionmenu4')
 const { functionmenu5 } = require('./menu/functionmenu5')
 const { hellomessage, errormessage } = require('./reply-message/replytext')
 
@@ -64,6 +64,8 @@ app.post('/webhook', (req, res) => {
         resulttimebus(req.body)
     }else if(req.body.events[0].message.text === 'ตารางค่าโดยสาร') {
         functionmenu4(req.body)
+    } else if(req.body.events[0].message.text === 'ราคารถแดง') {
+        costminibus(req.body)
     } else if(req.body.events[0].message.text === 'ราคารถเมล์') {
         selectnumbus(req.body)
     }else if(req.body.events[0].message.text === 'ราคารถเมล์ปอ.140') {
@@ -74,9 +76,16 @@ app.post('/webhook', (req, res) => {
         cost105(req.body)
     }else if(req.body.events[0].message.text === 'ราคารถเมล์ปอ.558') {
         cost558(req.body)
-    }     
-    else if(req.body.events[0].message.text === 'ราคารถเมล์ปอ.141') {
+    }else if(req.body.events[0].message.text === 'ราคารถเมล์ปอ.141') {
         cost141(req.body)
+    }else if(req.body.events[0].message.text === 'ราคารถเมล์ปอ.147') {
+        cost147(req.body)
+    }else if(req.body.events[0].message.text === 'ราคารถเมล์ปอ.68') {
+        cost68(req.body)
+    }else if(req.body.events[0].message.text === 'ราคารถเมล์ปอ.101') {
+        cost101(req.body)
+    }else if(req.body.events[0].message.text === 'ราคารถเมล์ปอ.720') {
+        cost720(req.body)
     }else if(req.body.events[0].message.text === 'ประวัติ') {
         functionmenu5(req.body)
     }
@@ -88,49 +97,6 @@ app.post('/webhook', (req, res) => {
     }
     });
 
-app.post('/webhook', (req, res) => {
-        if (req.body.events[0].message.type !== 'text') {
-            return;
-        }
-        if(req.body.events[0].message.text === 'สอบถามเส้นทาง') {
-            functionmenu1(req.body)
-        } else if(req.body.events[0].message.text === 'บางมด') {
-            menu1ans(req.body)
-        }else if(req.body.events[0].message.text === 'เช็กจุดขึ้นรถ') {
-            functionmenu2(req.body)
-        }else if(req.body.events[0].message.text === 'เลือกจุดเอง') {
-            custompoint(req.body)
-        } else if(req.body.events[0].message.text === 'ตารางเดินรถ') {
-            functionmenu3(req.body)
-        } else if(req.body.events[0].message.text === 'ตารางเวลารถเมล์') {
-            timebus(req.body)
-        }else if(req.body.events[0].message.text === 'ปอ.21') {
-            resulttimebus(req.body)
-        }else if(req.body.events[0].message.text === 'ตารางค่าโดยสาร') {
-            functionmenu4(req.body)
-        } else if(req.body.events[0].message.text === 'ราคารถเมล์') {
-            selectnumbus(req.body)
-        }else if(req.body.events[0].message.text === 'ราคารถเมล์ปอ.140') {
-            cost140(req.body)
-        }else if(req.body.events[0].message.text === 'ราคารถเมล์ปอ.76') {
-            cost76(req.body)
-        }else if(req.body.events[0].message.text === 'ราคารถเมล์ปอ.105') {
-            cost105(req.body)
-        }else if(req.body.events[0].message.text === 'ราคารถเมล์ปอ.558') {
-            cost558(req.body)
-        }     
-        else if(req.body.events[0].message.text === 'ราคารถเมล์ปอ.141') {
-            cost141(req.body)
-        }else if(req.body.events[0].message.text === 'ประวัติ') {
-            functionmenu5(req.body)
-        }
-        else if(req.body.events[0].message.text === 'สวัสดี') {
-            hellomessage(req.body)
-        }
-         else {
-            errormessage(req.body)
-        }
-        });
 // app.post('/webhook', (req, res) => {
 //     if (req.body.events[0].message.type !== 'text') {
 //         return;
