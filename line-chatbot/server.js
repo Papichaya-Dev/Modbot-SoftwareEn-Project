8237126e8a6443644bf7bfc5e3c25d5a1ed6f7e0
unknown <lo0kpad@hotmail.com>
2021-02-27 00:34:50 +0700

    :cherry_blossom:[Solved] CheckbusStop with destination & clear doc CheckbusStop :cherry_blossom:

--- line-chatbot/server.js
+++ line-chatbot/server.js
@@ -239,7 +239,9 @@ app.post('/webhook', (req, res) => {
                                }
                         })
                         .then(async data => {
+                            console.log('5555555555555555555', data)
                             let calData = {
+                                userId: data.userId,
                                 startLatitude: data.startLatitude,
                                 startLongitude: data.startLongitude,
                                 endLongitude: req.body.events[0].message.longitude , 
@@ -248,7 +250,7 @@ app.post('/webhook', (req, res) => {
                              BusData.find().then(async data => {
                                 let num = 0
                                 Promise.all(data.map(async doc => {
-                                    let docPromise = doc.bus_stop.map((busStop) => {
+                                    let docStartPromise = doc.bus_stop.map((busStop) => {
                                         return {
                                             bus_stop_name : busStop.bus_stop_name,
                                             cal_from_start : calcurateDistance(calData.startLatitude, calData.startLongitude, busStop.latitude, busStop.longitude, 'K'),
@@ -259,22 +261,49 @@ app.post('/webhook', (req, res) => {
                                          
                                     })
 
-                                     let testReturn = await Promise.all(docPromise)
-                                        .then((data) => {
+                                    let docEndPromise = doc.bus_stop.map((busStop) => {
+                                        return {
+                                            bus_stop_name : busStop.bus_stop_name,
+                                            cal_from_end : calcurateDistance(calData.endLatitude, calData.endLongitude, busStop.latitude, busStop.longitude, 'K'),
+                                            bus_no : doc.bus_no,
+                                            how_to_go: busStop.how_to_go
+
+                                        }
+                                         
+                                    })
+
+                                    
+
+                                     let testStartReturn = await Promise.all(docStartPromise)
+                                        .then(async (data) => {
                                             let sortData = data.sort((a, b) => a.cal_from_start - b.cal_from_start)
                                             console.log(sortData)
                                             // testSend(req.body, sortData[0].cal_from_start)
-                                            return sortData[0]
+                                            let mostFar = await Promise.all(docEndPromise)
+                                                .then((endData) => {
+                                                    let sortEndData = endData.sort((a, b) => a.cal_from_end - b.cal_from_end)
+                                                    console.log('endddddddddddddd', sortEndData[0])
+                                                    return sortEndData[0].cal_from_end
+                                                })
+                                            if(parseFloat(mostFar) <= 1) {
+                                                return sortData[0]
+                                            } else {
+                                                return "SoFar"
+                                            }
+                                            
                                         })
                                         .catch((err) => {
                                             console.log(err)
                                             return res.json({error: err})
                                         })
-                                    return testReturn
+                                    return testStartReturn
                                 }))
                                 .then((resData) => {
                                     console.log(resData)
                                     resultCheckBusStop(req.body, resData)
+                                    console.log('Prepare test delete', calData.userId)
+                                    CheckBusStop.deleteOne({userId : calData.userId}).then(() => console.log('delete complete'))
+                                    
                                 })
                             })
                             console.log(calDisStart21)
